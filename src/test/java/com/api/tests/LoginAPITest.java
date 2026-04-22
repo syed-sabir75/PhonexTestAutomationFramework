fd7a6175ebf3560e1277180e0b22cf0e8a7a9258
Syed v Sabir <sabirsyed430@gmail.com>
2026-04-22 13:44:37 +0300

    Till Requirement 3

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,49 @@
+package com.api.tests;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.api.pojo.UserCredentials;
+import static com.api.utils.ConfigManager.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class LoginAPITest {
+	
+	@Test
+	public void loginAPITest() throws IOException {
+		
+		
+		UserCredentials userCredentials = new UserCredentials("iamfd", "password");
+		
+		given()
+		     .baseUri(getProperty("BASE_URI"))
+		.and()
+		.contentType(ContentType.JSON)
+		.and()
+		.accept(ContentType.JSON)
+		.and()
+		.body(userCredentials)
+		.log().uri()
+		.log().method()
+		.log().headers()
+		.log().body()
+		.when()
+		.post("login")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.time(lessThan(7000L))
+		.and()
+		.body("message", equalTo("Success"))
+		.and()
+		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+	}
+
+}
---
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -0,0 +1,49 @@
+package com.api.tests;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+
+import static com.api.constant.Role.*;
+
+import static com.api.utils.AuthTokenProvider.*;
+
+import static com.api.utils.ConfigManager.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class UserDetailsAPITest {
+
+	@Test
+	public void userDetailsAPITest() throws IOException {
+
+		Header authHeader = new Header("Authorization", getToken(FD));
+
+		given()
+		.baseUri(getProperty("BASE_URI"))
+		.and().header(authHeader)
+		.and()
+		.accept(ContentType.JSON)
+				
+		.log().uri()
+		.log().method()
+		.log().body()
+		.log().headers()
+		.when()
+		.get("userdetails")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.time(lessThan(4500L))
+		.and()
+		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/UserDetailsResponseSchema.json"));
+
+	}
+
+}
