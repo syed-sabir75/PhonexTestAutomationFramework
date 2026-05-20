ba883729f0087468fbba83c04b056f1bb2d990be
Syed v Sabir <sabirsyed430@gmail.com>
2026-05-20 20:55:13 +0300

    Fixed the issue of generating the random problem ids for Create job payload

--- src/test/java/com/api/utils/FakerDataGenerator.java
+++ src/test/java/com/api/utils/FakerDataGenerator.java
@@ -26,7 +26,7 @@ public class FakerDataGenerator {
     private final static int MST_MODEL_ID = 1;
     
 
-
+    private final static int validProblemsId[] = {1,2,3,4,5,6,7,8,9,10,11,12,15,16,17,19,20,22,24,26,27,28,29};
 	private FakerDataGenerator() {
 
 	}
@@ -56,10 +56,10 @@ public class FakerDataGenerator {
 	}
 
 	private static List<Problems> generateFakeProblemsList() {
-		int problemId=RANDOM.nextInt(25)+1;
+		int randomIndex=RANDOM.nextInt(validProblemsId.length);
 		String fakerRemark = faker.lorem().sentence(10);
 		
-		Problems problems = new Problems(problemId, fakerRemark);
+		Problems problems = new Problems(validProblemsId[randomIndex], fakerRemark);
 		
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
