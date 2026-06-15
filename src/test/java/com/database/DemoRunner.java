58ad26a60edf75ed1981464d8a1c6840d74ecbb3
Syed v Sabir <sabirsyed430@gmail.com>
2026-06-15 22:59:30 +0300

    Update the DatabaseManager to use HikaryCP Library!!!

--- src/test/java/com/database/DemoRunner.java
+++ src/test/java/com/database/DemoRunner.java
@@ -6,10 +6,10 @@ public class DemoRunner {
 	public static void main(String[] args) throws SQLException {
 		
 		for(int i=1; i<=1000; i++) {
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
-		DatabaseManager.createConnection();
+		DatabaseManagerOLD.createConnection();
+		DatabaseManagerOLD.createConnection();
+		DatabaseManagerOLD.createConnection();
+		DatabaseManagerOLD.createConnection();
 	}
 
 }
