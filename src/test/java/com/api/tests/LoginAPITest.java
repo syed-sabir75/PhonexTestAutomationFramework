bd4234fa24e8bca8d6f4a7f3297a4050a2dbed45
Syed v Sabir <sabirsyed430@gmail.com>
2026-07-27 00:40:32 +0300

    Added Listeners to the test

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -7,12 +7,14 @@ import static org.hamcrest.Matchers.equalTo;
 import java.io.IOException;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.api.request.model.UserCredentials;
 import com.api.services.AuthService;
 import com.dataproviders.api.bean.UserBean;
 
+@Listeners(com.listerners.APITestListerner.class)
 public class LoginAPITest {
 	
 	private UserBean userCredentials; 
