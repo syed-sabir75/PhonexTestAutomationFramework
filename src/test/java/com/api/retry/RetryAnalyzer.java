91494a76d8323095ec3bcc892e720c40e4634e71
Syed v Sabir <sabirsyed430@gmail.com>
2026-08-09 22:27:34 +0300

    Added Retry Analyzer into the Framework
    Created RetryAnalyzer Class which will rerun failed test dynamically

---
+++ src/test/java/com/api/retry/RetryAnalyzer.java
@@ -0,0 +1,22 @@
+package com.api.retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+	private static final int MAX_ATTEMPTS = 2;
+	private int count = 1;
+
+	@Override
+	public boolean retry(ITestResult result) {
+
+		if (count <= MAX_ATTEMPTS) {
+			count++;
+			return true;
+		}
+
+		return false;
+	}
+
+}
